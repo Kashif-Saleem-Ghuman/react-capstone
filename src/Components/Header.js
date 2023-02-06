ec77c901da0ea93ee2276afa82ee250fd3a93f4b
unknown <kashif_saleem_23@hotmail.com>
2023-02-06 21:43:57 +0500

    add header component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import {
+  faLessThan, faMicrophone, faGear,
+} from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => (
-  <div>
-    <FontAwesomeIcon icon={faCoffee} />
-    Header
-  </div>
+  <>
+    <div className="nav-bar">
+      <div className="date-icon">
+        <span className="less-than-symbol"><FontAwesomeIcon icon={faLessThan} /></span>
+        <span className="year">
+          2023
+        </span>
+      </div>
+
+      <div className="weather">
+        Today&apos;s Weather
+      </div>
+      <div className="mic-icon-wrapper">
+        <span className="mic"><FontAwesomeIcon icon={faMicrophone} /></span>
+        <span className="gear"><FontAwesomeIcon icon={faGear} /></span>
+      </div>
+    </div>
+
+  </>
 );
 
 export default Header;

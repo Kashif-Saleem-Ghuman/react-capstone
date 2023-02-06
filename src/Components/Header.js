a884bc7b825aa2c00c59c71fadccf20e2db2c5a6
unknown <kashif_saleem_23@hotmail.com>
2023-02-06 20:41:52 +0500

    fix linters

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => (
   <div>
+    <FontAwesomeIcon icon={faCoffee} />
     Header
   </div>
 );

9a3256a969c3f4e5a62cef896215aa52a6a90684
unknown <kashif_saleem_23@hotmail.com>
2023-02-08 16:24:42 +0500

    add snapshot test for hero.test.js

--- src/__test__/Input.test.js
+++ src/__test__/Input.test.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import store from '../redux/store';
-import Hero from '../Components/Hero';
+import Input from '../Components/Input';
 
 describe('testing for input', () => {
   it('test for component', () => {
     const test = render(
       <Provider store={store}>
-        <BrowserRouter>
-          <Hero />
-        </BrowserRouter>
-
+        <Input />
       </Provider>,
     );
     expect(test).toMatchSnapshot();
